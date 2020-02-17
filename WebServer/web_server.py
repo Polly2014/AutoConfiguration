cc3ac9e288fc7a1748ffbc562f5578e916f063f2
Polly <26716201@qq.com>
2020-02-17 17:28:22 +0800

    Web Server with Flask

---
+++ WebServer/web_server.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+from flask import Flask, request, jsonify
+from plugins import License
+
+app = Flask(__name__)
+
+license = License('lksjfoianfoijwef')
+
+
+@app.route('/regist', methods=['POST', 'GET'])
+def regist():
+    if request.method == 'GET':
+        sn = request.args.get('sn')
+        username = request.args.get('username')
+        email = request.args.get('email')
+    data = {'license': license.make_lincense(sn)}
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    # app.debug = True  # 设置调试模式，生产模式的时候要关掉debug
+    app.run(host='0.0.0.0', port='80', debug=True)
