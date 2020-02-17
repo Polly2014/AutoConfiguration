86c4fcdd88a8e0e063a701eb0eae614ecd2e00a6
Polly <26716201@qq.com>
2020-02-17 17:31:50 +0800

    Web Server with Flask

---
+++ WebServer/plugins.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+
+import base64
+from Crypto.Cipher import AES
+import json
+
+
+class License(object):
+    def __init__(self, key):
+        if len(key) > 32:
+            key = key[:32]
+        self.key = self.to_16(key)
+
+    def to_16(self, key):
+        key = bytes(key, encoding="utf8")
+        while len(key) % 16 != 0:
+            key += b'\0'
+        return key  # 返回bytes
+
+    def aes(self):
+        return AES.new(self.key, AES.MODE_ECB)  # 初始化加密器
+
+    def make_lincense(self, text):
+        aes = self.aes()
+        return str(base64.encodebytes(aes.encrypt(self.to_16(text))),
+                   encoding='utf8').replace('\n', '')  # 加密
+
+    def valid_license(self, text):
+        aes = self.aes()
+        return str(aes.decrypt(base64.decodebytes(bytes(
+            text, encoding='utf-8'))).rstrip(b'\0').decode("utf-8"))  # 解密
